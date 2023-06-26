a6b206d5011bac05f145d95d0f621af6c3536868
Qokelley36 <113967985+Qokelley36@users.noreply.github.com>
2023-06-25 22:04:26 -0600

    Update Pytech_update.py

--- Module_6/Pytech_update.py
+++ Module_6/Pytech_update.py
@@ -1,1 +1,7 @@
+import pymongo
+from pymongo import MongoClient
+client = MongoClient("mongodb+srv://admin:admin@cluster0.lsm8cpk.mongodb.net/?retryWrites=true&w=majority")
+db = client["pytech"]
+collection = db["student"]
 
+collection.update_one ({"_id":1007}, {"$set" : {"last Name": "Danger"}})
